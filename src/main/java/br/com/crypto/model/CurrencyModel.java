b38363b52b1c1362ff95acbe879f8a9ff7513903
Wesley Ruben Oliveira da Silva <wesleysilva@ciandt.com>
2022-04-13 02:44:24 -0300

    Correcao do Atribudo createdAt da class CurrencyModel

--- src/main/java/br/com/crypto/model/CurrencyModel.java
+++ src/main/java/br/com/crypto/model/CurrencyModel.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.OffsetDateTime;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -21,5 +22,5 @@ public class CurrencyModel {
     private String Code;
 
     @CreationTimestamp
-    private String cratedAt;
+    private OffsetDateTime cratedAt;
 }
