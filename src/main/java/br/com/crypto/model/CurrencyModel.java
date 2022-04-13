220ae0f317f52fe81d59cfd68b248dcb1f864b7e
Wesley Ruben Oliveira da Silva <wesleysilva@ciandt.com>
2022-04-13 01:48:49 -0300

    Criando package model e class

---
+++ src/main/java/br/com/crypto/model/CurrencyModel.java
@@ -0,0 +1,28 @@
+package br.com.crypto.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class CurrencyModel {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String Code;
+
+    @CreationTimestamp
+    private String cratedAt;
+}
